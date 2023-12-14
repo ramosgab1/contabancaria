607fb0f6f466071a64ae088bccae3532dafb3a1b
ramosgab1 <gabrieller_david@hotmail.com>
2023-12-14 14:38:44 -0300

    Adicionados: Update Conta, Menu, Cores.

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,10 +1,82 @@
 package conta;
 
+import java.util.Scanner;
+
+import conta.util.Cores;
+
 public class Menu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+// Inicializar Scanner para input do usuário.
+		int opcoes=0; 
+		Scanner leia = new Scanner(System.in);
+		
+// Criação do Menu - do... while // switch case. Jacque comi palhacitos hoje, perdão. 
+
+		do { 
+		System.out.println(Cores.TEXT_YELLOW_BOLD + Cores.ANSI_BLACK_BACKGROUND
+				+ "-------------------------------------------");
+		System.out.println("                                           ");
+		System.out.println("           BANCO DA GENERATION             ");
+		System.out.println("           sempre com você! <3             ");
+		System.out.println("                                           ");
+		System.out.println("-------------------------------------------");
+		System.out.println("                                           ");
+		System.out.println("    1 - Criar conta                        ");
+		System.out.println("    2 - Listar todas as contas             ");
+		System.out.println("    3 - Buscar conta por número            ");
+		System.out.println("    4 - Atualizar dados da conta           ");
+		System.out.println("    5 - Apagar conta                       ");
+		System.out.println("    6 - Sacar                              ");
+		System.out.println("    7 - Depositar                          ");
+		System.out.println("    8 - Transferir valores entre contas    ");
+		System.out.println("    9 - Sair                               ");
+		System.out.println("                                           ");
+		System.out.println("-------------------------------------------");
+		System.out.println("ENTRE COM A OPCÃO DESEJADA:                " + Cores.TEXT_RESET);
+		opcoes=leia.nextInt();
+		
+		switch (opcoes) { 
+		case 1: 
+		System.out.println(Cores.TEXT_WHITE_BOLD + "    OPÇÃO: 1 - Criar conta                 \n");
+		break;
+		case 2:
+		System.out.println(Cores.TEXT_WHITE_BOLD + "    OPÇÃO: 2 - Listar todas as contas      \n");
+	    break; 
+		case 3:
+		System.out.println(Cores.TEXT_WHITE_BOLD + "    OPÇÃO: 3 - Buscar conta por número     \n");
+		break; 
+		case 4:
+		System.out.println(Cores.TEXT_WHITE_BOLD + "    OPÇÃO: 4 - Atualizar dados da conta    \n");
+		break;
+		case 5: 
+		System.out.println(Cores.TEXT_WHITE_BOLD + "    OPÇÃO: 5 - Apagar conta                \n");
+		break; 
+		case 6: 
+		System.out.println(Cores.TEXT_WHITE_BOLD + "    OPÇÃO: 6 - Saque                       \n");
+		break; 
+		case 7:
+		System.out.println(Cores.TEXT_WHITE_BOLD + "    OPÇÃO: 7 - Depósito                    \n");
+		break; 
+		case 8: 
+		System.out.println(Cores.TEXT_WHITE_BOLD + "    OPÇÃO: 8 - Transferir valor entre contas\n");
+		break;
+		case 9: 
+		System.out.println(Cores.TEXT_WHITE_BOLD + "    OPÇÃO: 9 - Sair                          \n");
+		break; 
+		default: 
+		System.out.println(Cores.TEXT_RED_BOLD + Cores.ANSI_BLACK_BACKGROUND + "    OPÇÃO INVÁLIDA! Tente outra vez!       \n");
+		}
+		
+		
+	} while (opcoes !=9);  
+		
+		if (opcoes == 9) { 
+		System.out.println(Cores.TEXT_YELLOW_BOLD +  Cores.ANSI_BLACK_BACKGROUND +"    Obrigado pela preferência! <3          ");
+		System.out.println(Cores.TEXT_GREEN + "       BANCO DA GENERATION                 ");
+		System.out.println(Cores.TEXT_YELLOW_BOLD + "-------------------------------------------");
+		}
 
 }
+} 
---
+++ src/conta/model/Conta.java
@@ -0,0 +1,82 @@
+package conta.model;
+
+public class Conta {
+
+// Atributos do objeto (pontos do template para serem preenchidos!)
+
+	private int numero;
+	private int agencia;
+	private int tipo;
+	private String titular;
+	private float saldo;
+
+// Trazemos os mesmos elementos dos atributos para que possamos ter acesso a eles.
+// Método construtor, o identificador após o this. faz referência aos atributos. 
+
+	public Conta(int numero, int agencia, int tipo, String titular, float saldo) {
+		this.numero = numero;
+		this.agencia = agencia;
+		this.tipo = tipo;
+		this.titular = titular;
+		this.saldo = saldo;
+	}
+// Aqui vamos ter GET e SET que fazem uma ponte entre o public e o private para manipular os dados. 
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public float getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
+
+// Para que possamos visualizar os dados da conta ao chamar a classe e colocar os argumentos em outro arquivo. 
+
+	public void visualizar() {
+
+		System.out.println("--------------------- INFORMAÇÕES DE CONTA ---------------------");
+		System.out.println("\n---------------------------- NÚMERO ----------------------------");
+		System.out.println("O número da conta é: " + this.numero);
+		System.out.println("-----------------------------------------------------------------");
+		System.out.println("A agência é: " + this.agencia);
+		System.out.println("-----------------------------------------------------------------");
+		System.out.println("O tipo da conta é: " + this.tipo);
+		System.out.println("-----------------------------------------------------------------");
+		System.out.println("O titular da conta é: " + this.titular);
+		System.out.println("-----------------------------------------------------------------");
+		System.out.println("O saldo disponível na conta é de: " + this.saldo);
+
+	}
+}
